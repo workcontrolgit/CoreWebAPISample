3002cbeb95d3e9de414b86ee8612de0c98f8c270
Fuji <fuji.nguyen@workcontrol.com>
2022-12-23 01:27:12 -0500

    Clean up program.cs

--- CoreWebAPI/Program.cs
+++ CoreWebAPI/Program.cs
@@ -1,42 +1,31 @@
 using AutoWrapper;
 using Serilog;
-
 try
 {
     var builder = WebApplication.CreateBuilder(args);
-
+    // setup serilog
     Log.Logger = new LoggerConfiguration()
-                    .ReadFrom.Configuration(builder.Configuration)
-                    .Enrich.FromLogContext()
-                    .CreateLogger();
+        .ReadFrom.Configuration(builder.Configuration)
+        .Enrich.FromLogContext()
+        .CreateLogger();
     builder.Host.UseSerilog(Log.Logger);
-
     // Add services to the container.
-
     builder.Services.AddControllers();
     // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
-
-
     var app = builder.Build();
-
     // Configure the HTTP request pipeline.
     if (app.Environment.IsDevelopment())
     {
         app.UseSwagger();
         app.UseSwaggerUI();
     }
-
     app.UseHttpsRedirection();
-
     app.UseAuthorization();
-
     app.MapControllers();
+    // setup AutoWrapper
     app.UseApiResponseAndExceptionWrapper();
-
-
-
     app.Run();
 }
 catch (Exception ex)
